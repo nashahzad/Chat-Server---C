35906d8c83d34454f9e49e5816f9f6bdfedaaa15
raylam <raylam@cs.stonybrook.edu>
2016-04-13 08:17:44 -0700

    server now accepts supported commands

--- hw5/server.c
+++ hw5/server.c
@@ -1,4 +1,20 @@
-#include "server.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <termios.h>
+#include <errno.h>
+#include <ctype.h>
+#include <netinet/in.h>
+#include <pthread.h>
+
+#define _GNU_SOURCE
+#define MAX_INPUT 1024
+#define USAGE "./server [-h|-v] PORT_NUMBER MOTD\n-h            Displays help menu & returns EXIT_SUCCESS.\n-v            Verbose print all incoming and outgoing protocol verbs and content.\nPORT_NUMBER   Port number to listen on.\nMOTD          Message to display to the client when they connect.\n"
 
 void * handleClient(void * param) {
   int * parameter = (int *) param;
@@ -50,12 +66,14 @@ int main(int argc, char *argv[]) {
     portNumber = atoi(argv[1]);
     strcpy(message, argv[2]);
   }
+
   //create socket
   int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
   if (serverSocket == -1) {
     printf("Failed to make server socket.\n");
     exit(EXIT_FAILURE);
   }
+
   //then bind it to a port
   struct sockaddr_in serverInfo, clientInfo;
   memset(&serverInfo, 0, sizeof(serverInfo));
@@ -67,23 +85,66 @@ int main(int argc, char *argv[]) {
     printf("Failed to bind to port %d\n", portNumber);
     exit(EXIT_FAILURE);
   }
+
+  //create the fd_set to monitor multiple file descriptors, including stdin to look for server commands
+  fd_set activeFdSet, readFdSet;
+  FD_ZERO(&activeFdSet);
+  FD_SET(serverSocket, &activeFdSet);
+  FD_SET(0, &activeFdSet);
+
   //now listen for any connection requests
   listen(serverSocket, 20); //arbitrary queue length
   printf("Currently listening on port %d\n", portNumber);
+  int counter;
   while(1) {
-    //accept an incoming connection
-    unsigned int clientLength;
-    write(1, "test\n", 5);
-    int clientSocket = accept(serverSocket, (struct sockaddr *) &clientInfo, &clientLength);
-    if (clientSocket == -1) {
-      printf("Accept error.\n");
-      continue;
+    write(1, ">", 1);
+    readFdSet = activeFdSet;
+    if (select(FD_SETSIZE, &readFdSet, NULL, NULL, NULL) == -1) {
+      //something bad happened to select
+
     }
-    //create thread
-    pthread_t tid;
-    pthread_create(&tid, NULL, (void *) &handleClient, (void *) &clientSocket);
-
-    //write(clientSocket, message, strlen(message));
-
+    for(counter = 0; counter < FD_SETSIZE; ++counter) {
+      if (FD_ISSET(counter, &readFdSet)) {
+        if (counter == serverSocket) {
+          //accept an incoming connection
+          unsigned int clientLength;
+          write(1, "test\n", 5);
+          int clientSocket = accept(serverSocket, (struct sockaddr *) &clientInfo, &clientLength);
+          if (clientSocket == -1) {
+            printf("Accept error.\n");
+            continue;
+          }
+          //create thread
+          pthread_t tid;
+          pthread_create(&tid, NULL, (void *) &handleClient, (void *) &clientSocket);
+        }
+        //is there something on stdin for the server?
+        else if (counter == 0) {
+          char test[MAX_INPUT] = {0};
+          fgets(test, MAX_INPUT - 1, stdin);
+          if (strncmp("/help\n", test, 6) == 0) {
+            printf("--------------------------------------\nCOMMAND LIST\n/users          Prints list of current users\n/help           Prints this prompt\n/shutdown       Shuts down server\n--------------------------------------\n");
+            fflush(stdout);
+          }
+          else if (strncmp("/users\n", test, 7) == 0) {
+            printf("/users test");
+            fflush(stdout);
+          }
+          else if (strncmp("/shutdown\n", test, 10) == 0) {
+            printf("/shutdown test");
+            fflush(stdout);
+          }
+          else {
+            test[strlen(test) - 1] = '\0';
+            printf("Unknown command %s. Type /help for more information.\n", test);
+            fflush(stdout);
+          }
+        }
+        else {
+          close(counter);
+          FD_CLR(counter, &activeFdSet);
+        }
+      }
+    }
   }
 }
