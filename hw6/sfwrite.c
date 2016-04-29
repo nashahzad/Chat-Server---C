7a606fa44a891758a1c2a21c858ec0bc01e7c8d9
nashahzad <nashahzad@cs.stonybrook.edu>
2016-04-29 13:35:38 -0400

    changed ordering of lock placement and unlocking in sfwrite.c

--- hw6/sfwrite.c
+++ hw6/sfwrite.c
@@ -6,12 +6,12 @@ void sf_write(pthread_mutex_t *lock, FILE *stream, char *fmt, ...){
 	va_start(list, fmt);
 
 	//LOCK IT FIRST THEN WRITE TO FILE
-	pthread_mutex_lock(lock);
 	flock(fileno(stream), LOCK_SH);
+	pthread_mutex_lock(lock);
 	
 		vfprintf(stream, fmt, list);
 	
 	//THEN IMMEDIATELY UNLOCK THE LOCK AFTER WRITING TO FILE
-	pthread_mutex_unlock(lock);
 	flock(fileno(stream), LOCK_UN);
+	pthread_mutex_unlock(lock);
 }
